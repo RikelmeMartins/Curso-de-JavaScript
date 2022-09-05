5cd50f314a5c1c57e35a51965af1e47e08c51648
Rikelme Martins <rikelme2005@gmail.com>
2022-09-05 16:58:54 -0300

    Aula12

---
+++ Aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 12
+if (idade < 16){
+    console.log("Não vota!")
+}
+else if (idade < 18 || idade >= 67){
+    console.log("Voto opicional!")
+}
+else{
+    console.log("Voto obrigatorio!")
+}
